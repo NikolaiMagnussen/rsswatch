09c09f5d0c1a846c1cd58198de5b6d7ea4d17001
Nikolai Magnussen <nikolai@magnussen.tf>
2019-05-04 12:42:46 +0200

    Add support for supplying name and directory in json file

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,40 @@
-use regex::Regex;
 use pipers::Pipe;
-use std::env::args;
+use regex::Regex;
 use rss::{Channel, Item};
+use serde::Deserialize;
+use std::env::args;
+use std::fs::File;
+use std::io::prelude::*;
 use std::process::Command;
+
+#[derive(Deserialize, Debug)]
+struct TorrentEntry {
+    name: String,
+    directory: String,
+}
 
 fn get_rss_feed(url: &str) -> Channel {
     Channel::from_url(url).unwrap()
 }
 
-fn match_items(items: Vec<Item>, title: &str) -> Vec<Item> {
-    items.into_iter().filter(|item| item.title().unwrap().contains(title)).collect()
+fn match_item_title(item: &Item, title: &str) -> bool {
+    match item.title() {
+        Some(t) => t.contains(title),
+        None => false,
+    }
+}
+
+fn match_items(items: Vec<Item>, title: &str) -> Result<Item, &'static str> {
+    let matched: Vec<Item> = items
+        .into_iter()
+        .filter(|item| match_item_title(item, title))
+        .collect();
+
+    match matched.len() {
+        0 => Err("No matched items"),
+        1 => Ok(matched[0].clone()),
+        _ => Err("Too many matches"),
+    }
 }
 
 fn start_torrent(torrent_name: &str) {
@@ -30,7 +55,11 @@ fn start_torrent(torrent_name: &str) {
 
 fn link_directory(torrent_name: &str, directory: &str) {
     Command::new("ln")
-        .args(&["-l", &format!("{}/{}", directory, torrent_name), "$HOME/Downloads"])
+        .args(&[
+            "-l",
+            &format!("{}/{}", directory, torrent_name),
+            "$HOME/Downloads",
+        ])
         .status()
         .expect("Failed to link directory");
 }
@@ -38,28 +67,49 @@ fn link_directory(torrent_name: &str, directory: &str) {
 fn extract_torrent_name(item: &Item) -> String {
     let link = item.link().unwrap().to_string();
     let re = Regex::new(r".*/(.*).torrent$").unwrap();
-    let cap = re.captures(&link)
+    let cap = re
+        .captures(&link)
         .expect("Unable to capture output")
         .get(1)
         .expect("Unable to extract torrent name");
     String::from(cap.as_str())
 }
 
-fn start_torrents(items: Vec<Item>, directory: &str) {
-    for item in items.into_iter() {
-        let name = extract_torrent_name(&item);
-        println!("\nGot name \"{}\"", name);
-        //link_directory(&name, directory);
-        //start_torrent(&name);
+fn start_torrents(torrent_entries: Vec<TorrentEntry>, items: Vec<Item>) {
+    for entry in torrent_entries.into_iter() {
+        match match_items(items.to_vec(), &entry.name) {
+            Ok(item) => {
+                let name = extract_torrent_name(&item);
+                println!(
+                    "Matched against \"{}\" and got name \"{}\"",
+                    entry.name, name
+                );
+                link_directory(&name, &entry.directory);
+                start_torrent(&name);
+            }
+            Err(e) => println!("Error \"{}\": {}", entry.name, e),
+        }
     }
+}
+
+fn get_torrent_and_directory(file: &str) -> Result<Vec<TorrentEntry>, std::io::Error> {
+    let mut contents = String::new();
+    File::open(file)?.read_to_string(&mut contents)?;
+
+    let entries: Vec<TorrentEntry> = serde_json::from_str(&contents)?;
+
+    Ok(entries)
 }
 
 fn main() {
     let url = args().nth(1).unwrap();
-    let search = "Bryggen";
+    let torrent_entries = get_torrent_and_directory("torrents.json");
+    match torrent_entries {
+        Ok(torrent_entries) => {
+            let stuff = get_rss_feed(&url);
 
-    let stuff = get_rss_feed(&url);
-    let a = match_items(stuff.items().to_vec(), search);
-
-    start_torrents(a, "/");
+            start_torrents(torrent_entries, stuff.items().to_vec());
+        }
+        Err(e) => println!("Error getting torrents and directory {:?}", e),
+    }
 }
